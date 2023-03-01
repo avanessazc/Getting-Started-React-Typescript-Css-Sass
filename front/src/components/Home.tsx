149894b606a50d3cd1fbe48b63654a529c09272d
ayzapata <ayzapata@student.42.fr>
2023-03-01 20:15:21 +0100

    passing props

--- front/src/components/Home.tsx
+++ front/src/components/Home.tsx
@@ -1,27 +1,17 @@
 import { useState } from 'react'
-
-type Blog = {
-  id: number
-  title: string
-  body: string
-  author: string
-}
+import { Blog } from '../types'
+import BlogList from './BlogList'
 
 const Home = () => {
   const [blogs, setBlogs] = useState<Blog[]>([
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
-  ]);
+  ])
 
   return (
     <div className='home'>
-      {blogs.map((blog: Blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Wittend by {blog.author}</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title='All Blogs!' />
     </div>
   )
 }
