7999d079acb41669b0274dc64a09dc8c19e628bd
ayzapata <ayzapata@student.42.fr>
2023-03-01 22:56:10 +0100

    conditional Loading Message

--- front/src/components/Home.tsx
+++ front/src/components/Home.tsx
@@ -4,19 +4,25 @@ import BlogList from './BlogList'
 
 const Home = () => {
   const [blogs, setBlogs] = useState<Blog[]>([])
-  
+  const [isPending, setIsPending] = useState<boolean>(true)
+
   useEffect(() => {
-    fetch('http://0.0.0.0:3000/blogs')
-    .then((res) => {
-      // console.log('res:', res);
-      return res.json();
-    }).then((data) => {
-      console.log(data);
-      setBlogs(data);
-    })
+    setTimeout(() => {
+      fetch('http://0.0.0.0:3000/blogs')
+        .then((res) => {
+          // console.log('res:', res);
+          return res.json()
+        })
+        .then((data) => {
+          console.log(data)
+          setBlogs(data)
+          setIsPending(false)
+        })
+    }, 1000)
   }, [])
   return (
     <div className='home'>
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title='All Blogs!' />}
     </div>
   )
