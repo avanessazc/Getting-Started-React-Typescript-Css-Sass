ec3c74ab4c291d77c2989dbc159d2c649b8fced3
ayzapata <ayzapata@student.42.fr>
2023-03-02 03:06:05 +0100

    useEffect Cleanup

--- front/src/hooks/useFetch.tsx
+++ front/src/hooks/useFetch.tsx
@@ -7,26 +7,32 @@ const useFetch = (url: string) => {
   const [error, setError] = useState<string>('')
 
   useEffect(() => {
+    const abortCtrl = new AbortController();
+
     setTimeout(() => {
-      fetch(url)
+      fetch(url, {signal: abortCtrl.signal})
         .then((res) => {
           if (!res.ok) {
-            throw Error('Could not fetch the data for that resource')
+            throw Error('Could not fetch the data for that resource');
           }
           return res.json()
         })
         .then((data) => {
-          console.log(data)
-          setData(data)
-          setIsPending(false)
-          setError('')
+          console.log(data);
+          setData(data);
+          setIsPending(false);
+          setError('');
         })
         .catch((err) => {
-          // console.log(err.message);
-          setError(err.message)
-          setIsPending(false)
+          if (err.name === 'AbortError') {
+            console.log('fetch aborted');
+          } else {
+            setError(err.message);
+            setIsPending(false);
+          }
         })
     }, 1000)
+    return () => abortCtrl.abort();
   }, [])
   return { data, isPending, error }
 }
