e15c21c0caeed89ac6f22503331cea11de76e552
ayzapata <ayzapata@student.42.fr>
2023-03-02 14:17:58 +0100

    Delete blogs

--- front/src/components/BlogDetails.tsx
+++ front/src/components/BlogDetails.tsx
@@ -1,15 +1,24 @@
-import { useParams } from 'react-router-dom'
+// import { MouseEvent } from 'react'
+import { useParams, useHistory } from 'react-router-dom'
 import useFetch from './hooks/useFetch'
 import { Blog } from '../types'
 
 const BlogDetails = () => {
   const { id } = useParams<{ id: string }>()
-
   const {
     data: blog,
     isPending,
     error,
   }: { data: Blog; isPending: boolean; error: string } = useFetch('http://0.0.0.0:3000/blogs/' + id)
+  const history = useHistory()
+  const handleDelete = () => {
+    fetch('http://0.0.0.0:3000/blogs/' + blog.id, {
+      method: 'DELETE'
+    })
+    .then(() => {
+      history.push('/')
+    })
+  }
   return (
     <div className='blog-details'>
       {isPending && <div>Loading...</div>}
@@ -19,6 +28,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleDelete}>Delete</button>
         </article>
       )}
     </div>
