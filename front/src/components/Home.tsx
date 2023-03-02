272cd497caf1669216b636a1a7952b3ab0852791
ayzapata <ayzapata@student.42.fr>
2023-03-02 02:20:54 +0100

    Custom Hook

--- front/src/components/Home.tsx
+++ front/src/components/Home.tsx
@@ -1,39 +1,15 @@
-import { useState, useEffect } from 'react'
-import { Blog } from '../types'
+// import { useState, useEffect } from 'react'
+// import { Blog } from '../types'
 import BlogList from './BlogList'
+import useFetch from '../hooks/useFetch'
 
 const Home = () => {
-  const [blogs, setBlogs] = useState<Blog[]>([])
-  const [isPending, setIsPending] = useState<boolean>(true)
-  const [error, setError] = useState<string>('');
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch('http://0.0.0.0:3000/blogss')
-        .then((res) => {
-          if (!res.ok) {
-            throw Error('Could not fetch the data for that resource');
-          }
-          return res.json()
-        })
-        .then((data) => {
-          console.log(data);
-          setBlogs(data);
-          setIsPending(false);
-          setError('');
-        })
-        .catch((err) => {
-          // console.log(err.message);
-          setError(err.message);
-          setIsPending(false);
-        })
-    }, 1000)
-  }, [])
+  const { data: blogs, isPending, error } = useFetch('http://0.0.0.0:3000/blogs')
   return (
     <div className='home'>
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs.length != 0 && <BlogList blogs={blogs} title='All Blogs!' />}
-      {error && <div>{error}</div>}
     </div>
   )
 }
